ae3f5f4c8adaf7a93acac9a903032945a41d69f8
Emmanuel Rideau <emmanuel.rideau@gmail.com>
2022-05-18 22:43:18 +0200

    Mise en place des événements

---
+++ src/Event/PurchaseSuccessEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Event;
+
+use App\Entity\Purchase;
+use Symfony\Contracts\EventDispatcher\Event;
+
+
+class PurchaseSuccessEvent extends Event
+{
+    private $purchase;
+
+    public function __construct(Purchase $purchase)
+    {
+        $this->purchase = $purchase;
+    }
+
+    public function getPurchase(): Purchase
+    {
+        return $this->purchase;
+    }
+}
